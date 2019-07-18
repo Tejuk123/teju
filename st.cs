e3596861b1b6871bd930ec9f068270f432518311
Tejuk123 <53030637+Tejuk123@users.noreply.github.com>
2019-07-18 10:44:08 +0530

    Add files via upload

---
+++ st.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace st
+{
+    class P
+    {
+        public static string t;
+        static string te;
+        static P()
+        {
+            t = "teju";
+            te = "te";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(P.t);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
