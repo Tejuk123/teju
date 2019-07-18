ffa5bbe2a79ce1da3f012ca7baad1bd5370bee1f
Tejuk123 <53030637+Tejuk123@users.noreply.github.com>
2019-07-18 10:49:25 +0530

    Add files via upload

---
+++ stk.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace te
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new List<string> { "<name>", "AMv", "Fel" };
+            foreach(var n in v)
+            {
+                Console.WriteLine($"Hello{n.ToUpper()}!");
+            }
+            Console.WriteLine();
+            v.Add("tej");
+            v.Remove("Fel");
+            foreach (var n in v)
+            {
+                Console.WriteLine($"Hello{n.ToUpper()}!");
+            }
+            Console.WriteLine(v.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
